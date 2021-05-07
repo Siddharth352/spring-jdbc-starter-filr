cc4e18d26ea01be65837a447f97c134fe907da57
siddharth singh <sidd15032000@gmail.com>
2021-05-07 17:08:16 +0530

    without xml file

--- src/main/java/com/spring/jdbc/App.java
+++ src/main/java/com/spring/jdbc/App.java
@@ -1,6 +1,7 @@
 package com.spring.jdbc;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -16,7 +17,7 @@ public class App
     {
 		System.out.println("Here program start");
     	
-		 ApplicationContext context = new ClassPathXmlApplicationContext("config.xml"); 
+		ApplicationContext context = new AnnotationConfigApplicationContext(ConfigClass.class); 
     	
     	studentDaoimple studentimpclass = context.getBean("studentdaoimple",studentDaoimple.class);
     	
---
+++ src/main/java/com/spring/jdbc/ConfigClass.java
@@ -0,0 +1,47 @@
+package com.spring.jdbc;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import com.spring.studentDaoImplement.studentDaoimple;
+import com.spring.studentDaoInterface.studentDao;
+
+@Configuration
+public class ConfigClass {
+	
+	@Bean("ds")
+	public DriverManagerDataSource generateds() {	
+		
+		DriverManagerDataSource ds = new DriverManagerDataSource();
+		ds.setDriverClassName("com.mysql.jdbc.Driver");
+		ds.setUrl("jdbc:mysql://localhost:3306/root");
+		ds.setUsername("root");
+		ds.setPassword("");		
+		return ds;
+		
+	}
+	
+	@Bean("jdbcTemplate")
+	public JdbcTemplate generateJdbctemp() {
+		
+		JdbcTemplate template = new JdbcTemplate(generateds());
+		
+		return template;
+	}
+	
+	
+	@Bean("studentdaoimple")
+	public studentDao generatestudentdao() {		
+		studentDaoimple sdi = new studentDaoimple();
+		sdi.setTemplate(generateJdbctemp());
+		return sdi;		
+		
+	}
+	
+	
+	
+	
+
+}
